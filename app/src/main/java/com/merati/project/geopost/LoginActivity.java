d6b6fe7a24f302a3087c68fe0daf0b090dce4b7b
francesco merati <francesmera@gmail.com>
2017-11-12 19:41:05 +0100

    Finished navigation bar and hamburger, added transitions between fragments

--- app/src/main/java/com/merati/project/geopost/LoginActivity.java
+++ app/src/main/java/com/merati/project/geopost/LoginActivity.java
@@ -1,12 +1,10 @@
 package com.merati.project.geopost;
 
-import android.app.IntentService;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -64,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
     public void startBrowsing(){
         if (sessionId!= null){
             logged= true;
-            Intent intent = new Intent(this, ActivityFollowed.class);
+            Intent intent = new Intent(this, MainActivity.class);
             intent.putExtra("session", sessionId);
             startActivity(intent);
         }
