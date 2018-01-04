c3af50805e58e2448f9c15be5f8fd13e6839e501
Francesco Merati <francesmera@gmail.com>
2018-01-04 15:48:09 +0100

    Added dialog to request localization permission in the UpdateStatus page (is it coeherent?). Now there are some problems with google play services that need fixing

--- app/src/main/java/com/merati/project/geopost/UpdateStatus.java
+++ app/src/main/java/com/merati/project/geopost/UpdateStatus.java
@@ -1,63 +1,51 @@
 package com.merati.project.geopost;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.LocationSource;
 
-public class UpdateStatus extends AppCompatActivity{
+public class UpdateStatus extends AppCompatActivity implements com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
     Model myModel = Model.getInstance();
     String status = null;
     Location locationUpdate = null;
+    GoogleApiClient mGoogleApiClient = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_status);
 
-        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-        LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                locationUpdate = location;
-                Log.d("UpdateStatus: ", "onLocationChanged");
-                if(status!= null){
-                    myModel.updateStatus(status, locationUpdate);
-                }
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-            }
-        };
-        try {
-            Log.d("UpdateStatus: ", "requesting location updates");
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-        catch(SecurityException se){
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
         }
+        mGoogleApiClient.connect();
+
+
     }
 
     public void updateStatus(View view){
@@ -72,5 +60,37 @@ public class UpdateStatus extends AppCompatActivity{
             status=msg;
         }
         Log.d("UpdateStatus" , "Button update clicked; msg: "+status+" location: "+ locationUpdate);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        locationUpdate = location;
+        Log.d("UpdateStatus: ", "onLocationChanged");
+        if(status!= null){
+            myModel.updateStatus(status, locationUpdate);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("UpdateStatus : ", "Google api client connected");
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("UpdateStatus", "checking permission...");
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
